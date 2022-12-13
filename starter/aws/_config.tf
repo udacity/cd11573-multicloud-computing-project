2ff72d80c644bbc8d31324063453086b0294e2a3
Travis Scotto <travisscotto@macbook-pro-2.mynetworksettings.com>
2022-12-12 22:15:20 -0500

    Adding AWS

---
+++ starter/aws/_config.tf
@@ -0,0 +1,7 @@
+  provider "aws" {
+   region = "us-east-2"
+   
+   default_tags {
+     tags = local.tags
+   }
+ }
---
+++ starter/aws/main.tf
@@ -0,0 +1,17 @@
+locals {
+   name   = "udacity"
+   region = "us-east-2"
+   tags = {
+     Name      = local.name
+     Terraform = "true"
+   }
+ }
+
+ module "vpc" {
+   source     = "./modules/vpc"
+   cidr_block = "10.100.0.0/16"
+
+   account_owner = local.name
+   name          = "${local.name}-project"
+   azs           = ["us-east-2a", "us-east-2b", "us-east-2c"]
+ }
